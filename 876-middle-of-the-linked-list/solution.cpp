bf4a327b235c4b55d920fc991708f24d23e431a9
pvilayan <158111956+pvilayan@users.noreply.github.com>
2025-06-16 17:32:22 +0800

    Update solution.cpp

--- 876-middle-of-the-linked-list/solution.cpp
+++ 876-middle-of-the-linked-list/solution.cpp
@@ -1,1 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int counter = 0;
+        ListNode*current = head;
+        ListNode*tracker = current;
+        while (current!= nullptr){
+            counter++;
+            current = current -> next;
+        }
+        int middle = counter/2;
+        for (int i=0; i<middle; i++){
+            tracker = tracker->next;
+            
+        }
+        return tracker;
+        }
 
+        };
+
+
+
+
+        
+    
