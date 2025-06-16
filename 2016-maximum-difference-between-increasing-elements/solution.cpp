a68fa5165b2051dd59285a67ce9243623fb0aeb6
pvilayan <158111956+pvilayan@users.noreply.github.com>
2025-06-16 23:43:55 +0800

    Update solution.cpp

--- 2016-maximum-difference-between-increasing-elements/solution.cpp
+++ 2016-maximum-difference-between-increasing-elements/solution.cpp
@@ -16,3 +16,22 @@ public:
     }
     
 };
+
+#Another Solution
+
+class Solution {
+public:
+    int maximumDifference(vector<int>& nums) {
+        int max_diff = -1 ;
+        int min_val = nums[0];
+        for(int i = 1; i<nums.size(); i++){
+            if (nums[i]>min_val){
+                max_diff = max(max_diff, nums[i]-min_val);
+            }
+            else{
+                min_val = nums[i];
+            }
+        }
+        return max_diff;
+    }
+    };
